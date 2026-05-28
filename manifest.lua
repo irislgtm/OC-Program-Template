d3d6c830bf89d49199547f36397f0cedcf4790a4
irislgtm <irislgtm@users.noreply.github.com>
2026-05-28 22:13:34 +0000

    deploy: 722991059ab835121cd008239c75e8ed2cd387d7

--- manifest.lua
+++ manifest.lua
@@ -1,9 +1,7 @@
 -- auto-generated
 return {
-  {path="/bin/hello.lua",size=701},
-  {path="/bin/robot_mine.lua",size=818},
   {path="/bin/update.lua",size=1872},
-  {path="/etc/rc.cfg",size=44},
-  {path="/etc/rc.d/example.lua",size=174},
-  {path="/lib/args.lua",size=542}
+  {path="/etc/.gitkeep",size=0},
+  {path="/home/.gitkeep",size=0},
+  {path="/lib/.gitkeep",size=0}
 }
